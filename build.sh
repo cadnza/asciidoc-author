bb6f624ec808d9e63ec73f8688a0bc7551968ef1
Jon Dayley <jonathandayley@gmail.com>
2021-10-23 07:10:01 -0600

    Add build script for testing

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+# Go to git directory
+cd $(git -C $(dirname $0) rev-parse --show-toplevel)
+
+# Package extension
+final=./test.vsix
+vsce package -o $final
+
+# Uninstall old extension
+extId=cadnza.asciidoc-author-highlighting
+code --uninstall-extension $extId
+
+# Install new extension
+code --install-extension $final
